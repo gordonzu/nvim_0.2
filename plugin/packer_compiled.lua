4ac432e9d4d3ece8f1d88a3c67459a36ac474872
gordonzu <gordonzu@outlook.com>
2024-12-10 12:52:58 -0500

    nvim-dap-ui

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -167,10 +167,20 @@ _G.packer_plugins = {
     path = "/home/gordonz/.local/share/nvim/site/pack/packer/start/nvim-dap",
     url = "https://github.com/mfussenegger/nvim-dap"
   },
+  ["nvim-dap-ui"] = {
+    loaded = true,
+    path = "/home/gordonz/.local/share/nvim/site/pack/packer/start/nvim-dap-ui",
+    url = "https://github.com/rcarriga/nvim-dap-ui"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/home/gordonz/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-nio"] = {
+    loaded = true,
+    path = "/home/gordonz/.local/share/nvim/site/pack/packer/start/nvim-nio",
+    url = "https://github.com/nvim-neotest/nvim-nio"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
